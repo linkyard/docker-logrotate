108d97bae056fdd76481f647134d5169214e6c68
Ferdinand Hübner <ferdinand.huebner@linkyard.ch>
2019-10-01 07:09:11 +0200

    Fix logrotate cronjob execution

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,7 +19,7 @@ ts "${TS_FORMAT}" < /etc/logrotate.conf
 
 if [ -d "/etc/periodic/${LOGROTATE_CRON:-15min}" ]; then
   echo "using /etc/periodic/${LOGROTATE_CRON:-15min} cron schedule" | ts "${TS_FORMAT}"
-  mv /etc/.logrotate.cronjob "/etc/periodic/${LOGROTATE_CRON:-15min}"
+  mv /etc/.logrotate.cronjob "/etc/periodic/${LOGROTATE_CRON:-15min}/logrotate"
 else
   echo "assuming \"${LOGROTATE_CRON:-15min}\" is a cron expression; appending to root's crontab" | ts "${TS_FORMAT}"
   echo "${LOGROTATE_CRON:-15min} /etc/.logrotate.cronjob" >> /var/spool/cron/crontabs/root
